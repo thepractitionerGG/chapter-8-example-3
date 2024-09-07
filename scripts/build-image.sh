6967331568b00da1e00cb510763242a82baa9f76
Vikram <thepractitioner.gg@gmail.com>
2024-09-07 20:21:15 +1000

    First Commit

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -15,4 +15,4 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-docker build -t $CONTAINER_REGISTRY/video-streaming:$VERSION --file ./Dockerfile-prod .
+docker build -t $CONTAINER_REGISTRY/video-streaming:1 --file ./Dockerfile-prod .
